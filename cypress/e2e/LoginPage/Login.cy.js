3a5441591011f14664ec8c92f6fb266f37a098e3
javedansari278 <javedansari278@gmail.com>
2022-09-27 14:34:51 +0530

    Updated

--- cypress/e2e/LoginPage/Login.cy.js
+++ cypress/e2e/LoginPage/Login.cy.js
@@ -7,18 +7,18 @@ describe('Login Page suite',{tags:'@Login'},()=>{
     it('Enter username',()=>{
         cy.get('#username').type(Cypress.env('userID'))
         cy.log(Cypress.env('userID'))
-        cy.screenshot()
+        cy.screenshot({capture:'runner'})
     })
 
     it('Enter Password',()=>{
         cy.get('#password').type(Cypress.env('password'))
         cy.log(Cypress.env('password'))
-        cy.screenshot()
+        cy.screenshot({capture:'runner'})
     })
 
     it('Click on Login button',()=>{
         cy.get('[name="login"]').click()
         cy.get("[class='woocommerce-MyAccount-navigation'] li:nth-child(1) a").should('have.text','Dashboard')
-        cy.screenshot()
+        cy.screenshot({capture:'runner'})
     })
 })
